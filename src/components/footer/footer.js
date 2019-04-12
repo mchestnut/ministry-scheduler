c91bd9605280d262385fd6bf78b00699c84e8d13
Matt Chestnut <matt@mattchestnut.com>
2019-04-12 12:48:56 -0700

    Adds link to TBC website to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
 
     return (
       <footer className={ styles.footer }>
-        &copy; Copyright {today.getFullYear()} Tri-City Baptist Church Ministries
+        &copy; Copyright {today.getFullYear()} <a href="https://www.tricityministries.org" className={ styles.footer__link }>Tri-City Baptist Church Ministries</a>
       </footer>
     )
   }
